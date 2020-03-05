5c12c23f848cb453c134c9317feeef662f734980
松本和奈 <matsumotokazuna@matsumotowananoMacBook-Pro.local>
2020-03-05 21:13:20 +0900

    [add]fizz_buzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,10 +1,10 @@
 def fizz_buzz(number)
-    if number % 3 == 0
+    if number % 3 == 0 && number % 5 == 0
+        "FizzBuzz"
+    elsif number % 3 == 0
         "Fizz"
     elsif number % 5 == 0
         "Buzz"
-    elsif number % 3 == 0 && number % 5 == 0
-        "FizzBuzz"
     else
         number.to_s
     end
